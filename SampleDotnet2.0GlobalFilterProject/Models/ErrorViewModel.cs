52ac342e8c4691e14e97b676ec954788e7bd0057
Kevin Rich <krich@bookbyte.com>
2017-08-18 13:40:36 -0700

    Add project files.

---
+++ SampleDotnet2.0GlobalFilterProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SampleDotnet2._0GlobalFilterProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
