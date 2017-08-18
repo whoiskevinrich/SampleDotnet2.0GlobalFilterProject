e6f60a760cd2981664a2cad52ef8ee3c89aab968
Kevin Rich <krich@bookbyte.com>
2017-08-18 15:28:23 -0700

    Finsihed Startup configurations

--- SampleDotnet2.0GlobalFilterProject/Startup.cs
+++ SampleDotnet2.0GlobalFilterProject/Startup.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 namespace SampleDotnet2._0GlobalFilterProject
 {
@@ -30,10 +31,58 @@ namespace SampleDotnet2._0GlobalFilterProject
                 sharedOptions.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
                 sharedOptions.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
             })
+            // bind the "AzureAd" Section in appsettings.json to my OpenIdConnectOptions
             .AddAzureAd(options => Configuration.Bind("AzureAd", options))
-            .AddCookie();
+            .AddJwtBearer(options =>
+                {
+                    options.Audience = Configuration["JwtBearer.Audience"];
+                    options.Authority = Configuration["JwtBearer.Authority"];
+                } )
+            // bind the "AzureJwtBearer" Section in appsettings.json to my JwtBearerOptions
+            .AddAzureJwtBearer(options => Configuration.Bind("AzureJwtBearer", options))
+            // configure Cookie auth 
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/Account/SignIn";
+                    options.LogoutPath = "/Account/SignOut";
+                    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+                    options.Events = new CookieAuthenticationEvents
+                    {
+                        OnRedirectToLogin = OnRedirectToLogin
+                    };
+                });
+
+            // authorization
+            services.AddAuthorization(options =>
+            {
+                // require user to have cookie auth or jwt bearer token
+                options.AddPolicy("Authenticated",
+                    policy => policy
+                        .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme, CookieAuthenticationDefaults.AuthenticationScheme)
+                        .RequireAuthenticatedUser());
+            });
+
+            // Add global authorization policy
+            services.Configure<MvcOptions>(options =>
+            {
+                options.Filters.Add(new AuthorizeFilter("Authenticated"));
+            });
 
             services.AddMvc();
+        }
+
+        private static Task OnRedirectToLogin(RedirectContext<CookieAuthenticationOptions> context)
+        {
+            if (context.Request.Path.StartsWithSegments("/api"))
+            {
+                // return 401 if not "logged in" from an API Call
+                context.Response.StatusCode = (int) HttpStatusCode.Unauthorized;
+                return Task.CompletedTask;
+            }
+
+            // Redirect users to login page
+            context.Response.Redirect(context.RedirectUri);
+            return Task.CompletedTask;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
